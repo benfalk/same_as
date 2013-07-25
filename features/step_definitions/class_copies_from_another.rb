4592a06c25106287f65d72f3180a7aa9a7c98710
Benjamin Falk <bfalk@c-s-d.org>
2013-07-25 10:28:46 -0500

    Passing the initial setup of cucumber senario 'class copies from another'

--- features/step_definitions/class_copies_from_another.rb
+++ features/step_definitions/class_copies_from_another.rb
@@ -1,21 +1,21 @@
 Given(/^I have a MailingAddress model from billing info$/) do
   @billing = BillingInformation.new
-  @mailing = @billing.build_mailing_address address: 'just bounce',
+  @mailing = @billing.build_mailing_address street: 'just bounce',
                                             zip: '00844',
                                             state: 'awesome'
 end
 
-Given(/^I have specified a ShippingAddress model to be the same$/) do |arg1|
-  @shiping = @billing.build_shipping_address same_as_mailing: true
+Given(/^I have specified a ShippingAddress model to be the same$/) do
+  @shipping = @billing.build_shipping_address same_as_mailing: true
 end
 
-When(/^I save the ShippingAddress model$/) do |arg1|
-  @shiping.save!
+When(/^I save the ShippingAddress model$/) do
+  @shipping.save!
 end
 
-Then(/^the ShippingAddress fields should reflect the MailingAddress$/) do |arg1, arg2|
-  @shiping.address.should == @mailing.address
-  @shiping.zip.should == @mailing.zip
-  @shiping.state.should == @mailing.state
+Then(/^the ShippingAddress fields should reflect the MailingAddress$/) do
+  @shipping.street.should == @mailing.street
+  @shipping.zip.should == @mailing.zip
+  @shipping.state.should == @mailing.state
 end
 
--- test_models/billing_information.rb
+++ test_models/billing_information.rb
@@ -1,3 +1,11 @@
 class BillingInformation < ActiveRecord::Base
 
+  has_one :mailing_address,
+          :class_name => 'MailingAddress',
+          :as => :addressable
+
+  has_one :shipping_address,
+          :class_name => 'ShippingAddress',
+          :as => :addressable
+
 end
